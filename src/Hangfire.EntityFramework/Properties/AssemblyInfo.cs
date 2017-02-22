799844269bd8ebcbfb4cc46506bb601893523fc7
Sergey Zhigunov <serge.zhigunov@outlook.com>
2017-02-22 23:30:27 +0500

    Initial commit

---
+++ src/Hangfire.EntityFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hangfire.EntityFramework")]
+[assembly: AssemblyDescription("Entity Framework 6 job storage for Hangfire")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("8e6a464d-1ae3-49e1-8d2e-59fc76f015e0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: InternalsVisibleTo("Hangfire.EntityFramework.Tests")]
---
+++ test/Hangfire.EntityFramework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hangfire.EntityFramework.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fb221f6a-0e71-4a9f-91ac-4688444a0577")]
