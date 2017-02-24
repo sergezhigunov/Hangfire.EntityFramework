3989bb464afccec78f46665a37a2dbd138a33115
Sergey Zhigunov <serge.zhigunov@outlook.com>
2017-02-25 01:12:36 +0500

    Add Entity Framework job storage tests

---
+++ test/Hangfire.EntityFramework.Tests/EntityFrameworkJobStorageTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Hangfire.EntityFramework.Utils;
+using Xunit;
+
+namespace Hangfire.EntityFramework
+{
+    public class EntityFrameworkJobStorageTests
+    {
+        private EntityFrameworkJobStorageOptions Options;
+
+        public EntityFrameworkJobStorageTests()
+        {
+            Options = new EntityFrameworkJobStorageOptions();
+        }
+
+        [Fact]
+        public void Ctor_ThrowsAnException_WhenConnectionStringIsNull()
+        {
+            Assert.Throws<ArgumentNullException>("nameOrConnectionString",
+                () => new EntityFrameworkJobStorage(null));
+        }
+
+        [Fact]
+        public void Ctor_ThrowsAnException_WhenOptionsValueIsNull()
+        {
+           Assert.Throws<ArgumentNullException>("options",
+                () => new EntityFrameworkJobStorage(string.Empty, null));
+        }
+
+        [Fact, CleanDatabase]
+        public void GetMonitoringApi_ReturnsNonNullInstance()
+        {
+            var storage = CreateStorage();
+            var api = storage.GetMonitoringApi();
+            Assert.NotNull(api);
+        }
+
+        [Fact, CleanDatabase]
+        public void GetConnection_ReturnsNonNullInstance()
+        {
+            var storage = CreateStorage();
+            using (var connection = (EntityFrameworkJobStorageConnection)storage.GetConnection())
+            {
+                Assert.NotNull(connection);
+            }
+        }
+
+        private EntityFrameworkJobStorage CreateStorage() =>
+            new EntityFrameworkJobStorage(ConnectionUtils.GetConnectionString(), Options);
+    }
+}
