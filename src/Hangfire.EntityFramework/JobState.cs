5de46096dd951a0f8db2745c8b393a0718452c15
Sergey Zhigunov <serge.zhigunov@outlook.com>
2017-11-20 04:28:03 +0500

    Add JobState enumeration

---
+++ src/Hangfire.EntityFramework/JobState.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2017 Sergey Zhigunov.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+namespace Hangfire.EntityFramework
+{
+    internal enum JobState
+    {
+        Enqueued = 0,
+        Scheduled,
+        Processing,
+        Succeeded,
+        Failed,
+        Deleted,
+        Awaiting,
+    }
+}
